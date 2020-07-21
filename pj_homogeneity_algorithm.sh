ba62ca3a9a083e9f2a8d5e58415a900840b33eb3
Chloe Cheng <chloemt.cheng@mail.utoronto.ca>
2020-07-21 14:59:10 -0400

    Indication for red clumps

---
+++ pj_homogeneity_algorithm.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+homogeneity_algorithm()
+{
+	for i in 1 2 3 4 5 6 7 8 9 10
+	do
+		python3 /geir_data/scr/ccheng/AST425/Personal/pj_run_code.py --cluster=$1 --num_sigma=$2 --red_clump=$3 --run_number=${i} &
+	done
+}
+
+echo "Enter a cluster:"
+read x
+echo "Enter the number of simulations:"
+read y
+echo "Enter True if you want to remove red clump stars, False if not:"
+read z
+
+mkdir run_files
+cd run_files 
+cp /geir_data/scr/ccheng/AST425/Personal/pj_clusters_input.py /geir_data/scr/ccheng/AST425/Personal/run_files/pj_clusters_input.py
+cp /geir_data/scr/ccheng/AST425/Personal/occam_clusters_post_process.py /geir_data/scr/ccheng/AST425/Personal/run_files/occam_clusters_post_process.py
+cp /geir_data/scr/ccheng/AST425/Personal/pj_ABC.py /geir_data/scr/ccheng/AST425/Personal/run_files/pj_ABC.py
+cp /geir_data/scr/ccheng/AST425/Personal/pj_run_code.py /geir_data/scr/ccheng/AST425/Personal/run_files/pj_pjrun_code.py
+mkdir /geir_data/scr/ccheng/AST425/Personal/run_files/${x}
+
+homogeneity_algorithm $x $y $z 
