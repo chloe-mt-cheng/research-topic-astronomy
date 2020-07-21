76d5dba2d9bf5cc9e48b490dee6725b0ed8aa971
Chloe Cheng <chloemt.cheng@mail.utoronto.ca>
2020-07-21 14:55:59 -0400

    No longer saving cumulative distributions

--- occam_clusters_post_process.py
+++ occam_clusters_post_process.py
@@ -9,7 +9,7 @@ Examples:
 --element=<arg>  Element name
 """
 
-from docopt import docopt
+#Imports
 #apogee package 
 from apogee.tools.path import change_dr
 change_dr('14') #use DR14
@@ -20,6 +20,8 @@ import glob
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import os
+import time
+from docopt import docopt
 
 fs=16
 plt.rc('font', family='serif',size=fs)
@@ -46,45 +48,15 @@ def res_cat(residuals, errors):
 	all_err = np.concatenate(errors, axis=0)
 	return all_res, all_err
 
-def make_directory_cdist(cluster):
-	"""Create a new directory for the cumulative distributions for the cluster in question, 
-	if the directory does not already exist.
-	
-	Parameters 
-	----------
-	cluster : str
-		The name of the desired cluster (e.g. 'NGC 2682')
-	"""
-	
-	name_string = str(cluster).replace(' ', '') #Remove spaces from cluster name
-	
-	#If the directory exists, do nothing
-	#if glob.glob('/Users/chloecheng/Personal/' + name_string + '/' + 'cdist'): #Personal path
-	if glob.glob('/geir_data/scr/ccheng/AST425/Personal/' + name_string + '/cdist'): #Server path
-		return None
-	#If the directory does not exist, create a new one
-	else:
-		#os.makedirs('/Users/chloecheng/Personal/' + name_string + '/' + 'cdist') #Personal path
-		os.makedirs('/geir_data/scr/ccheng/AST425/Personal/' + name_string + '/cdist') #Server path
-
-def cum_dist(cluster, elem, dat_type, residuals, errors, sigma_val=None):
+def cum_dist(residuals, errors):
 	"""Return the cumulative distribution of the normalized residuals of the fit.
-	Also saves all cumulative distributions created to hdf5 files.
 	
 	Parameters
 	----------
-	cluster : str
-		The name of the desired cluster (e.g. 'NGC 2682')
-	elem : str
-		The name of the desired element (e.g. 'AL')
-	dat_type : str
-		Indicates whether the information being considered is from the data or a simulation (e.g. 'data' or 'sim')
 	residuals : tuple
 		Array of floats representing the residuals of the quadratic fits
 	errors : tuple
 		Array of floats representing the spectral errors corresponding to the residuals
-	sigma_val : float, optional
-		Indicates the value of sigma being used for the simulation in question, if applicable (default is None)
 	
 	Returns
 	-------
@@ -94,45 +66,12 @@ def cum_dist(cluster, elem, dat_type, residuals, errors, sigma_val=None):
 		One-dimensional array containing the sorted, normalized fit residuals
 	"""
 	
-	name_string = str(cluster).replace(' ','') #Remove spaces from cluster name
 	all_res, all_err = res_cat(residuals, errors) #Concatenate residuals and errors
 	num_res = len(all_res)
 	y_ax = np.linspace(0, 1, num_res)
 	cdist = np.sort(all_res/all_err)
 	
-	#Personal path
-	#path = '/Users/chloecheng/Personal/' + name_string + '/cdist/' + name_string + '_' + elem + '_' + str(dat_type) + '_' + 'cdist' + '.hdf5'
-	#Server path
-	path = '/geir_data/scr/ccheng/AST425/Personal/' + name_string + '/cdist/' + name_string + '_' + elem + '_' + str(dat_type) + '_' + 'cdist' + '.hdf5'
-	#If we are looking at the data
-	if sigma_val == None:
-		#If the cumulative distribution already exists, return y_ax and cdist
-		if glob.glob(path):
-			return y_ax, cdist
-		#If the cumulative distribution does not exist, write to file and return
-		else:
-			file = h5py.File(path, 'w')
-			file['cumulative_distribution'] = cdist
-			file['y_ax'] = y_ax
-			file.close()
-			return y_ax, cdist
-	#If we are looking at a simulation
-	else:
-		#If the cumulative distribution exists, append to the file with the value of sigma
-		if glob.glob(path):
-			file = h5py.File(path, 'a')
-			grp = file.create_group(str(sigma_val))
-			grp['cumulative_distribution'] = cdist
-			grp['y_ax'] = y_ax
-			file.close()
-		#If the file does not exist, create a new one
-		else: 
-			file = h5py.File(path, 'w')
-			grp = file.create_group(str(sigma_val))
-			grp['cumulative_distribution'] = cdist
-			grp['y_ax'] = y_ax
-			file.close()
-		return y_ax, cdist
+	return y_ax, cdist
 
 def cum_dist_plot(cluster, elem, residuals, errors, obj, axvline=False, sigma_val=None):
 	"""Plot the cumulative distribution of the normalized residuals.
@@ -239,8 +178,7 @@ def symm_plot(cluster, elem, residuals, errors, obj, axvline=False, sigma_val=No
 if __name__ == '__main__':
 	arguments = docopt(__doc__)
 	
-	residuals, errors = imports(arguments['--cluster'], arguments['--element'])
-	y_ax, cdist = cum_dist(arguments['--cluster'], arguments['--element'], residuals, errors)
+	y_ax, cdist = cum_dist(residuals, errors)
 	cdist_dir = make_directory_cdist(arguments['--cluster'])
 	elem_cdist_plot = cum_dist_plot(residuals, errors, arguments['--element'], arguments['--cluster'], axvline=False)
 	elem_symm_plot = symm_plot(residuals, errors, arguments['--element'], arguments['--cluster'], axvline=False)
