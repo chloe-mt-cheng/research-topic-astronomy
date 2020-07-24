52fb8b2ddb8f992da830eaf07411901e4d878ce1
Chloe Cheng <chloemt.cheng@mail.utoronto.ca>
2020-07-24 14:31:57 -0400

    Varying individual element and masking outside DR12 windows

--- pj_homogeneity_algorithm.sh
+++ pj_homogeneity_algorithm.sh
@@ -3,23 +3,27 @@ homogeneity_algorithm()
 {
 	for i in 1 2 3 4 5 6 7 8 9 10
 	do
-		python3 /geir_data/scr/ccheng/AST425/Personal/pj_run_code.py --cluster=$1 --num_sigma=$2 --red_clump=$3 --run_number=${i} &
+		python3 /geir_data/scr/ccheng/AST425/Personal/pj_run_code.py --cluster=$1 --num_sigma=$2 --red_clump=$3 --run_number=${i} --location=$4 --elem=$5 &
 	done
 }
 
 echo "Enter a cluster:"
-read x
+read a
 echo "Enter the number of simulations:"
-read y
+read b
 echo "Enter True if you want to remove red clump stars, False if not:"
-read z
+read c
+echo "Enter the run location:"
+read d
+echo "Enter an element:"
+read e
 
 mkdir run_files
 cd run_files 
 cp /geir_data/scr/ccheng/AST425/Personal/pj_clusters_input.py /geir_data/scr/ccheng/AST425/Personal/run_files/pj_clusters_input.py
 cp /geir_data/scr/ccheng/AST425/Personal/occam_clusters_post_process.py /geir_data/scr/ccheng/AST425/Personal/run_files/occam_clusters_post_process.py
 cp /geir_data/scr/ccheng/AST425/Personal/pj_ABC.py /geir_data/scr/ccheng/AST425/Personal/run_files/pj_ABC.py
-cp /geir_data/scr/ccheng/AST425/Personal/pj_run_code.py /geir_data/scr/ccheng/AST425/Personal/run_files/pj_pjrun_code.py
-mkdir /geir_data/scr/ccheng/AST425/Personal/run_files/${x}
+cp /geir_data/scr/ccheng/AST425/Personal/pj_run_code.py /geir_data/scr/ccheng/AST425/Personal/run_files/pj_run_code.py
+mkdir /geir_data/scr/ccheng/AST425/Personal/run_files/${a}
 
-homogeneity_algorithm $x $y $z 
+homogeneity_algorithm $a $b $c $d $e
