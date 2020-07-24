5f6d795b34645ed7aa237352f25ba2300355b0c3
Chloe Cheng <chloemt.cheng@mail.utoronto.ca>
2020-07-24 13:00:44 -0400

    Fixed directory-naming variable

--- homogeneity_algorithm.sh
+++ homogeneity_algorithm.sh
@@ -3,16 +3,20 @@ homogeneity_algorithm()
 {
 	for i in 1 2 3 4 5 6 7 8 9 10
 	do
-		python3 /geir_data/scr/ccheng/AST425/Personal/run_code.py --cluster=$1 --num_sigma=$2 --red_clump=$3 --run_number=${i} &
+		python3 /geir_data/scr/ccheng/AST425/Personal/run_code.py --cluster=$1 --num_sigma=$2 --red_clump=$3 --run_number=${i} --location=$4 --elem=$5 &
 	done
 }
 
 echo "Enter a cluster:"
-read x
+read a
 echo "Enter the number of simulations:"
-read y
+read b
 echo "Enter True if you want to remove red clump stars, False if not:"
-read z
+read c
+echo "Enter the run location:"
+read d
+echo "Enter an element:"
+read e
 
 mkdir run_files
 cd run_files 
@@ -20,6 +24,6 @@ cp /geir_data/scr/ccheng/AST425/Personal/occam_clusters_input.py /geir_data/scr/
 cp /geir_data/scr/ccheng/AST425/Personal/occam_clusters_post_process.py /geir_data/scr/ccheng/AST425/Personal/run_files/occam_clusters_post_process.py
 cp /geir_data/scr/ccheng/AST425/Personal/ABC.py /geir_data/scr/ccheng/AST425/Personal/run_files/ABC.py
 cp /geir_data/scr/ccheng/AST425/Personal/run_code.py /geir_data/scr/ccheng/AST425/Personal/run_files/run_code.py
-mkdir /geir_data/scr/ccheng/AST425/Personal/run_files/${x}
+mkdir /geir_data/scr/ccheng/AST425/Personal/run_files/${a}
 
-homogeneity_algorithm $x $y $z 
+homogeneity_algorithm $a $b $c $d $e
