28de2db4e2c00d7309b7ac5e566857c835844729
Chloe Cheng <chloemt.cheng@mail.utoronto.ca>
2020-08-04 17:25:13 -0400

    Running more sims

--- pj_homogeneity_algorithm.sh
+++ pj_homogeneity_algorithm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 homogeneity_algorithm()
 {
-	for i in 1 2 3 4 5 6 7 8 9 10
+	for i in {1..100}
 	do
 		python3 /geir_data/scr/ccheng/AST425/Personal/pj_run_code.py --cluster=$1 --num_sigma=$2 --red_clump=$3 --run_number=${i} --location=$4 --elem=$5 &
 	done
@@ -18,12 +18,12 @@ read d
 echo "Enter an element:"
 read e
 
-mkdir run_files
-cd run_files 
-cp /geir_data/scr/ccheng/AST425/Personal/pj_clusters_input.py /geir_data/scr/ccheng/AST425/Personal/run_files/pj_clusters_input.py
-cp /geir_data/scr/ccheng/AST425/Personal/occam_clusters_post_process.py /geir_data/scr/ccheng/AST425/Personal/run_files/occam_clusters_post_process.py
-cp /geir_data/scr/ccheng/AST425/Personal/pj_ABC.py /geir_data/scr/ccheng/AST425/Personal/run_files/pj_ABC.py
-cp /geir_data/scr/ccheng/AST425/Personal/pj_run_code.py /geir_data/scr/ccheng/AST425/Personal/run_files/pj_run_code.py
-mkdir /geir_data/scr/ccheng/AST425/Personal/run_files/${a}
+mkdir run_files_${a}_${e}
+cd run_files_${a}_${e}
+cp /geir_data/scr/ccheng/AST425/Personal/pj_clusters_input.py /geir_data/scr/ccheng/AST425/Personal/run_files_${a}_${e}/pj_clusters_input.py
+cp /geir_data/scr/ccheng/AST425/Personal/occam_clusters_post_process.py /geir_data/scr/ccheng/AST425/Personal/run_files_${a}_${e}/occam_clusters_post_process.py
+cp /geir_data/scr/ccheng/AST425/Personal/pj_ABC.py /geir_data/scr/ccheng/AST425/Personal/run_files_${a}_${e}/pj_ABC.py
+cp /geir_data/scr/ccheng/AST425/Personal/pj_run_code.py /geir_data/scr/ccheng/AST425/Personal/run_files_${a}_${e}/pj_run_code.py
+mkdir /geir_data/scr/ccheng/AST425/Personal/run_files_${a}_${e}/${a}
 
 homogeneity_algorithm $a $b $c $d $e
